cf4962d4c3c59efb0d19dacfa6f5fac69d6e37a7
André Oliveira <39243097+andrefpoliveira@users.noreply.github.com>
2020-09-05 19:41:01 +0100

    Update __init__.py

--- ldb/__init__.py
+++ ldb/__init__.py
@@ -33,7 +33,7 @@ def create(labels: str or list or tuple):
     global cols
     global idv
 
-    if not (type(labels) == str or type(labels) == list or type(labels) == tuple):
+    if not (type(labels) in [str,list,tuple]):
         raise Exception("Passed value is not a str or list or tuple")
     if type(labels) == str:
         labels = [labels]
@@ -51,7 +51,7 @@ def add_c(arg: str or list or tuple):
     global lbels
     global data
     global cols
-    if type(arg) == int or type(arg) == bool or type(arg) == str:
+    if type(arg) in [int,bool,str]:
         lbels.append(arg)
         nol = 1
     else:
@@ -72,7 +72,7 @@ def add_r(dat: tuple or list):
     global data
     global cols
     global idv
-    if type(dat) == int or type(dat) == str or type(dat) == bool:
+    if type(dat) in [int,str,bool]:
         dat = [dat]
         dat.insert(0, idv)
         data.append(dat)
